327cc57b67a04b9f0f5e171e9cacd2a9e7cc7ef9
Anthony Bonta <zthun.coding@gmail.com>
2018-07-23 18:48:36 -0500

    Exports module first.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
+export * from './z-identifier.module';
+
 export * from './z-attr/z-identifier-generator.service';
 export * from './z-attr/z-abstract-attr-directive.class';
 export * from './z-for/z-for.directive';
 export * from './z-id/z-id.directive';
-export * from './z-identifier.module';
